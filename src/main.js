d4e5b77ab1c71734cec88120ab18d915092df7d7
lyoung <lashellyoung15@gmail.com>
2024-05-21 18:19:21 -0500

    chegg skills course orientation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 
-const promise = welcome();
+// const promise = welcome();
+// console.log(promise);
 
-console.log(promise);
+// using promise response in then 
+// const question = "Will the weather be nice today?";
+// const tellPromise = tell(question);
+// tellPromise.then((result) => {
+//     console.log(question)
+//     console.log(result)
+// }).catch((err) => console.log(err))
+
+// catch error
+// const tellPromiseAndThrowError = tell()
+//     .then((result) => {
+//         console.log("Success:", result);
+//     })
+//     .catch((error) => {
+//         console.log("Failure:", error);
+//     });
+
+// setTimeout(() => console.log(tellPromiseAndThrowError), 500);
+
+// then & catch
+welcome()
+    .then(console.log) // Logs the result of welcome()
+    .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
+    .then(console.log) // Skipped because tell returned a rejected promise.
+    .catch(console.error) // Logs error from tell() or welcome()
+    .then(goodbye) // Returns promise from goodbye()
+    .then(console.log) // Logs the result from goodbye()
+    .catch(console.error); // Logs error only from goodbye()
